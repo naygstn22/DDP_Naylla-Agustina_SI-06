5601e1ff10e5a2ef1f5ccaf309ac3e7e9010c487
naygstn22 <0110124227@student.nurulfikri.ac.id>
2024-11-30 02:48:23 +0700

    Add files via upload

---
+++ ddp_3.py
@@ -0,0 +1,53 @@
+nama = "Naylla"
+nim = "0110124227"
+rombel = "SI 05"
+no_telp = "0895321617030"
+alamat = "bogor, jawa Barat"
+
+print(f"nama saya:{nama}")
+print(f"nim saya:{nim}")
+print(f"rombel saya:{rombel}")
+print(f"no_telp saya:{no_telp}")
+print(f"alamat saya:{alamat}")
+print(f"================")
+
+#nomer2
+
+nama = "Salwa Saleysia"
+nim ="0110124155"
+rombel = "SI 06"
+no_telp = "0881025570519"
+alamat = "Bogor, Jawa Barat"
+
+print(f"nama:{nama}")
+print(f"nim:{nim}")
+print(f"rombel:{rombel}")
+print(f"no_telp:{no_telp}")
+print(f"alamat:{alamat}")
+print("============")
+
+#nomer 3
+TB =int(input("masukin tinggi badan"))
+
+bb_ideal = (TB - 100)- (TB - 100) * 0,15
+
+print("berat badan ideal :",bb_ideal)
+print("====================")
+
+#nomer 4
+celcius = float(input("masukin nilai celcius:"))
+fahrenheit= 9/5 *celcius + 32
+
+print(f"suhu dalam fahrenheit adalah:{fahrenheit:}")
+print("==================")
+
+#nomer5
+phi=22/7
+r=int(input("masukkan jari-jari tabung"))
+t=int(input("masukkan tinggi tabung"))
+
+keliling=2 * 22/7 * r
+luas=keliling * (r+t)
+
+print("jadi luas kliling tabung adalah",keliling)
+print("jadi luas tabung adalah",luas)
